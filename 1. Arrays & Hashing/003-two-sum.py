38499419004b9b782da367893785e8fc12f357fc
Kalyan <kalyannagavenkat123@gmail.com>
2024-07-20 19:19:08 +0530

    Create 003-two-sum.py

---
+++ 1. Arrays & Hashing/003-two-sum.py
@@ -0,0 +1,50 @@
+'''
+Difficulty: Easy
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+'''
+
+#Method-1: Brute force
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+                
+#Time: O(n^2)
+#Spcae: O(1)
+
+#----------------------------------------------------------------------
+
+#Method-2: Using hashmap {value: index} (visit first and then store)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashmap:
+                return [i, hashmap[diff]]
+            hashmap[n] = i
+
+#Time: O(n)
+#Spcae: O(n)
